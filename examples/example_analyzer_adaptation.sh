f341200ffeec5723f05c0c301e2bc9cf6231ec9f
JoserraLP <jolozanop@alumnos.unex.es>
2021-12-28 14:13:54 +0100

    Added the remaining examples

---
+++ examples/example_analyzer_adaptation.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cd ../docker-utils/docker_generator && ../clean_up_containers.sh > /dev/null 2>&1; python main.py -o  ../../docker-compose.yml -c mosquitto,influxdb,grafana,telegraf,traffic_light_controller,traffic_analyzer && sudo docker-compose up
