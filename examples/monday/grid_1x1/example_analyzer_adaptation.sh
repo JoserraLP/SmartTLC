1e5ef2dd3bf3140f07501962a2e41ec3fd9ad5e9
JoserraLP <jolozanop@alumnos.unex.es>
2022-06-07 09:54:32 +0200

    Added same flows to 1x1 and 2x2 grid monday examples

--- examples/monday/grid_1x1/example_analyzer_adaptation.sh
+++ examples/monday/grid_1x1/example_analyzer_adaptation.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cwd=$(pwd)/flows.rou.xml
+
 # Move to docker generator folder
 cd ../../../docker-utils/docker_generator || exit
 
@@ -7,7 +9,7 @@ cd ../../../docker-utils/docker_generator || exit
 ../clean_up_containers.sh > /dev/null 2>&1 
 
 # Create docker-compose file with the time pattern
-python main.py -o  ../../docker-compose.yml -c traffic_analyzer,mosquitto,influxdb,grafana,telegraf,traffic_light_controller:pattern#/etc/traffic_light_controller/time_patterns/base_patterns/monday.csv:rows#3:cols#3:lanes#2
+python main.py -o  ../../docker-compose.yml -c traffic_analyzer,mosquitto,influxdb,grafana,telegraf,traffic_light_controller:pattern#/etc/traffic_light_controller/time_patterns/base_patterns/monday.csv:load#$cwd:rows#3:cols#3:lanes#2
 
 # Deploy the architecture
 sudo docker-compose up
