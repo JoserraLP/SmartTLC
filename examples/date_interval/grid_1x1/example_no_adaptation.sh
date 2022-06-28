fd64b6f9ec3bfa93c644187d366f7fa97d2a3e0a
JoserraLP <jolozanop@alumnos.unex.es>
2022-06-28 10:09:30 +0200

    Added random time pattern and date interval simulation examples

--- examples/date_interval/grid_1x1/example_no_adaptation.sh
+++ examples/date_interval/grid_1x1/example_no_adaptation.sh
@@ -9,7 +9,7 @@ cd ../../../docker-utils/docker_generator || exit
 ../clean_up_containers.sh > /dev/null 2>&1
 
 # Create docker-compose file with the time pattern
-python main.py -o  ../../docker-compose.yml -c traffic_analyzer,mosquitto,influxdb,grafana,telegraf,traffic_light_controller:pattern#/etc/traffic_light_controller/time_patterns/base_patterns/monday.csv:load#$cwd:rows#4:cols#4:lanes#2,traffic_light_predictor_context
+python main.py -o  ../../docker-compose.yml -c mosquitto,influxdb,grafana,telegraf,traffic_light_controller:date#18/07/2021-25/07/2021:load#$cwd:rows#3:cols#3:lanes#2
 
 # Deploy the architecture
 sudo docker-compose up
