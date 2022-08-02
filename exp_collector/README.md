185bb96db6aabe61321e8b41d791946cbe1b9d14
JoserraLP <jolozanop@alumnos.unex.es>
2022-08-02 12:00:37 +0200

    Experiment collector docstring

---
+++ exp_collector/README.md
@@ -0,0 +1,33 @@
+# Experiment collector
+Script for retrieving all the information related to a given experiment from the *data storage* component.
+
+## Usage
+The execution itself is performed by executing the next command
+
+```sh
+python main.py <parameters>
+```
+
+This execution process creates a file where there is stored all the traffic information related to an experiment.
+
+Where the parameters defined are:
+
+- **-h, --help**: show this help message and exit.
+- **-o OUTPUT_FILE, --output-file OUTPUT_FILE**: define the output file location. Must be an XLSX file. 
+  Default is ./data.xlsx.
+- **-w WAITING_TIME, --waiting-time WAITING_TIME**: waiting seconds to deploy. Use only when deploying Docker container 
+  because the experiment lasts several minutes. Default to 0.
+  
+## Data model
+The information stored in the output file uses the following model:
+- **date_day**: integer number representing the day.
+  - **date_month**: integer number representing the month.
+- **date_year**: integer number representing the year.
+- **hours**: it represents the current hour of the simulation. It stores information based on the defined temporal 
+  window of 5 minutes.
+- **passing_veh_e_w**: number of vehicles that have passed from both east and west.
+- **passing_veh_n_s**: number of vehicles that have passed from both north and south.
+- **waiting_time_veh_e_w**: amount of time the vehicles that have passed from both east and west.
+- **waiting_time_veh_n_s**: amount of time the vehicles that have passed from both north and south.
+- **junction**: junction identifier.
+- **average**: average value of waiting time per vehicle in all the junctions.
--- exp_collector/main.py
+++ exp_collector/main.py
@@ -1,38 +1,41 @@
-import numpy as np
-import pandas as pd
-from influxdb_client import InfluxDBClient
-import optparse
+import argparse
 import time
 import warnings
 
+import numpy as np
+import pandas as pd
+from influxdb_client import InfluxDBClient
+
+# Define constants values related to data retrieval and InfluxDB variables
 NUM_ITEMS_PER_DAY = 288
 TEMPORAL_WINDOW_MINUTES = 5
 DEFAULT_OUTPUT_FILE = "./data.xlsx"
-
-# You can generate a Token from the "Tokens Tab" in the UI
 TOKEN = "my-super-secret-auth-token"
 ORG = "smarttlc"
 BUCKET = "smarttlc"
-
-client = InfluxDBClient(url="http://172.20.0.3:8086", token=TOKEN)
-
-query_api = client.query_api()
+DB_URL = "http://172.20.0.3:8086"
 
 
 def get_options():
     """
-    Define options for the executable script.
+    Get options for the executable script.
 
-    :return: options
-    :rtype: object
+    :return: Arguments options
     """
-    optParser = optparse.OptionParser()
-    optParser.add_option("-o", "--output-file", dest="output_file", action='store',
-                         help="output file location")
-    optParser.add_option("-w", "--waiting-time", dest="waiting_time", action='store', default=0,
-                         help="waiting seconds to deploy. Use only when deploying Docker container. Default to 0")
-    options, args = optParser.parse_args()
-    return options
+    # Create the Argument Parser
+    arg_parser = argparse.ArgumentParser(description='Script to collect an experiment information and store it on a '
+                                                     'XLSX file.')
+
+    # Define the arguments options
+    arg_parser.add_argument("-o", "--output-file", dest="output_file", action='store', default=DEFAULT_OUTPUT_FILE,
+                            help=f"define the output file location. Must be an XLSX file. "
+                                 f"Default is {DEFAULT_OUTPUT_FILE}")
+    
+    arg_parser.add_argument("-w", "--waiting-time", dest="waiting_time", action='store', default=0,
+                            help="waiting seconds to deploy. Use only when deploying Docker container. Default to 0")
+    # Retrieve the arguments parsed
+    args = arg_parser.parse_args()
+    return args
 
 
 if __name__ == '__main__':
@@ -42,15 +45,16 @@ if __name__ == '__main__':
     # Retrieve execution options (parameters)
     exec_options = get_options()
 
-    output_file = DEFAULT_OUTPUT_FILE
-
-    if exec_options.output_file:
-        output_file = exec_options.output_file
-
-    # Sleep waiting time
+    # Sleep process specified waiting time
     time.sleep(int(exec_options.waiting_time))
 
-    # Query
+    # Define connection to InfluxDB database
+    client = InfluxDBClient(url=DB_URL, token=TOKEN)
+
+    # Retrieve the query InfluxDB API
+    query_api = client.query_api()
+
+    # Define the query
     query = 'from(bucket: "{}")\
             |> range(start: -24h)\
             |> filter(fn: (r) => r["_field"] == "waiting_time_veh_e_w" \
@@ -58,44 +62,48 @@ if __name__ == '__main__':
             or r["_field"] == "passing_veh_n_s" or r["_field"] == "date_day" \
             or r["_field"] == "date_month" or r["_field"] == "date_year")'
 
-    # using Table structure
-    tables = query_api.query_data_frame(query.format(BUCKET), org=ORG)
+    # Parsing the query results to a Pandas DataFrame
+    results = query_api.query_data_frame(query.format(BUCKET), org=ORG)
 
-    if not tables.empty:
-        # Unused columns
+    # Check if there are results
+    if not results.empty:
+        # Define unused columns
         unused_columns = ["result", "table", "_start", "_stop", "_time", "host", "topic"]
 
-        # Process the dataframe, removing unused columns
-        tables = tables.drop(columns=unused_columns)
+        # Removing unused columns
+        results = results.drop(columns=unused_columns)
 
         # Rename columns
         rename_columns = {'_value': 'value', '_field': 'field', '_measurement': 'junction'}
-
-        tables.rename(columns=rename_columns, inplace=True)
+        results.rename(columns=rename_columns, inplace=True)
 
         # Remove summary rows
-        tables = tables[tables['junction'] != 'summary']
+        results = results[results['junction'] != 'summary']
 
         # Get number of junctions
-        num_junctions = len(tables['junction'].unique())
+        num_junctions = len(results['junction'].unique())
 
-        # Retrieve dates dataframe
-        date_day = pd.concat([tables[tables['field'] == 'date_day']], ignore_index=True).reset_index()
-        date_month = pd.concat([tables[tables['field'] == 'date_month']], ignore_index=True).reset_index()
-        date_year = pd.concat([tables[tables['field'] == 'date_year']], ignore_index=True).reset_index()
+        # Retrieve dataframe per days, month and year
+        date_day = pd.concat([results[results['field'] == 'date_day']], ignore_index=True).reset_index()
+        date_month = pd.concat([results[results['field'] == 'date_month']], ignore_index=True).reset_index()
+        date_year = pd.concat([results[results['field'] == 'date_year']], ignore_index=True).reset_index()
 
-        # 288 items per day -> Get number of days
-        num_days = len(tables.loc[(tables['field'] == 'passing_veh_e_w')]) / (NUM_ITEMS_PER_DAY * num_junctions)
+        # 288 items per day -> Get number of days with information
+        num_days = len(results.loc[(results['field'] == 'passing_veh_e_w')]) / (NUM_ITEMS_PER_DAY * num_junctions)
 
         # Get indexes from data
         indexes = [int(x * NUM_ITEMS_PER_DAY * num_days) for x in range(num_junctions)]
 
         # Retrieve hours list and parse it to dataframe. Time range are each 5 minutes
         hours = []
-        for _ in range(int(num_days)*num_junctions):
+        # Iterate over the items of a day and a given junction
+        for _ in range(int(num_days) * num_junctions):
+            # Iterate over the hours of a day
             for x in range(24):
+                # Iterate over the minutes by the temporal window
                 for y in range(0, 60, TEMPORAL_WINDOW_MINUTES):
                     hour = "{x}:{y}"
+                    # Format the hour and minutes based on the data information
                     if x < 10 and y < 10:
                         hour = hour.format(x='0' + str(x), y='0' + str(y))
                     elif x < 10:
@@ -104,21 +112,24 @@ if __name__ == '__main__':
                         hour = hour.format(x=x, y='0' + str(y))
                     else:
                         hour = hour.format(x=x, y=y)
+                    # Append to the list of hours
                     hours.append(hour)
 
+        # Parse hour list to a DataFrame
         hours = pd.DataFrame(hours)
 
         # Get sum of passing vehicles EW and NS
-        passing_veh_ew = tables.loc[(tables['field'] == 'passing_veh_e_w')].reset_index()
-        passing_veh_ns = tables.loc[(tables['field'] == 'passing_veh_n_s')].reset_index()
+        passing_veh_ew = results.loc[(results['field'] == 'passing_veh_e_w')].reset_index()
+        passing_veh_ns = results.loc[(results['field'] == 'passing_veh_n_s')].reset_index()
 
         # Get sum of waiting time EW and NS
-        waiting_time_ew = tables.loc[(tables['field'] == 'waiting_time_veh_e_w')].reset_index()
-        waiting_time_ns = tables.loc[(tables['field'] == 'waiting_time_veh_n_s')].reset_index()
+        waiting_time_ew = results.loc[(results['field'] == 'waiting_time_veh_e_w')].reset_index()
+        waiting_time_ns = results.loc[(results['field'] == 'waiting_time_veh_n_s')].reset_index()
 
+        # Define DataFrame to store the average values
         average_waiting_time = pd.DataFrame(columns=['value'])
 
-        # Iterate over the indexes
+        # Iterate over the different number of items per day
         for i in range(NUM_ITEMS_PER_DAY):
             value = 0
             # Iterate over different junctions
@@ -134,7 +145,7 @@ if __name__ == '__main__':
             # Append the average waiting time value
             average_waiting_time = average_waiting_time.append({'value': value}, ignore_index=True)
 
-        # Replace inf values by 0
+        # Replace infinite values by 0
         average_waiting_time = average_waiting_time.replace(np.inf, 0)
 
         # Process the data to set columns by info
@@ -149,11 +160,11 @@ if __name__ == '__main__':
                                             passing_veh_ns['junction'],
                                             average_waiting_time['value']], axis=1))
         # Set the data columns
-        data.columns = ['date_day', 'date_month', 'date_year', 'hours', 'passing_veh_ew', 'passing_veh_ns',
-                        'waiting_time_veh_ew', 'waiting_time_veh_ns', 'junction', 'average']
+        data.columns = ['date_day', 'date_month', 'date_year', 'hours', 'passing_veh_e_w', 'passing_veh_n_s',
+                        'waiting_time_veh_e_w', 'waiting_time_veh_n_s', 'junction', 'average']
 
-        # Add average waiting time
+        # Calculate average waiting time mean value
         data['average_waiting_time'] = average_waiting_time['value'].mean()
 
-        # store the output excel
-        data.to_excel(output_file, index=None, header=True)
+        # Store the output file
+        data.to_excel(exec_options.output_file, index=None, header=True)
