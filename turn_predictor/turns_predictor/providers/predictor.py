a3b76df2abc6f0b0b7f624ee5d260f25aae1e56e
JoserraLP <jolozanop@alumnos.unex.es>
2022-09-21 12:33:11 +0200

    Distributed turn predictor

--- turn_predictor/turns_predictor/providers/predictor.py
+++ turn_predictor/turns_predictor/providers/predictor.py
@@ -1,22 +1,13 @@
 import ast
 import copy
+
 import paho.mqtt.client as mqtt
 import pandas as pd
+from sumo_generators.static.constants import MQTT_URL, MQTT_PORT, TRAFFIC_INFO_TOPIC, \
+    TURN_PREDICTION_TOPIC
+from sumo_generators.utils.utils import parse_str_to_valid_schema
 from turns_predictor.ml.model_predictor import ModelPredictor
-from turns_predictor.static.constants import MQTT_URL, MQTT_PORT, DEFAULT_NUM_MODELS, PREDICTION_TOPIC, \
-    TRAFFIC_INFO_TOPIC
-
-
-def parse_str_to_valid_schema(input_info: str) -> str:
-    """
-    Parse the input string to a valid middleware schema by replacing special characters
-
-    :param input_info: input string
-    :type input_info: str
-    :return: input string in valid format
-    :rtype: str
-    """
-    return str(input_info).replace('\'', '\"').replace(" ", "")
+from turns_predictor.static.constants import DEFAULT_NUM_MODELS
 
 
 class Predictor:
@@ -62,8 +53,8 @@ class Predictor:
         """
         # If connected successfully
         if rc == 0:
-            # Subscribe to the traffic info topic
-            self._mqtt_client.subscribe(TRAFFIC_INFO_TOPIC)
+            # Subscribe to the traffic info topic from all traffic lights -> Append #
+            self._mqtt_client.subscribe(TRAFFIC_INFO_TOPIC + '/#')
 
             # Load all the models when connecting to the middleware
             self._model_predictor.load_best_models(num_models=self._num_models)
@@ -80,54 +71,53 @@ class Predictor:
         # Parse to message input dict
         traffic_info = ast.literal_eval(msg.payload.decode('utf-8'))
 
+        # Retrieve junction id
+        junction_id = str(msg.topic).split('/')[1] if '/' in msg.topic else ''
+
         # Define the roads
         information_per_road = []
 
-        # Iterate over the traffic lights
-        for traffic_light_info in traffic_info:
+        # Check valid value
+        if junction_id != '':
+            # Convert the traffic information to dataframe
+            traffic_data = pd.DataFrame([list(traffic_info.values())], columns=list(traffic_info.keys()))
 
-            # Exclude summary information
-            if traffic_light_info['tl_id'] != 'summary':
-                traffic_light_info.pop("tl_id", None)
-                # Convert the traffic information to dataframe
-                traffic_data = pd.DataFrame([list(traffic_light_info.values())],
-                                            columns=list(traffic_light_info.keys()))
+            # Remove unused model features
+            traffic_data = traffic_data.drop(
+                labels=['actual_program', 'waiting_time_veh_e_w', 'waiting_time_veh_n_s', 'turning_vehicles',
+                        'day', 'passing_veh_n_s', 'passing_veh_e_w'], axis=1)
 
-                # Remove unused model features
-                traffic_data = traffic_data.drop(
-                    labels=['tl_program', 'waiting_time_veh_e_w', 'waiting_time_veh_n_s', 'turning_vehicles',
-                            'day', 'passing_veh_n_s', 'passing_veh_e_w'], axis=1)
+            # Parse date info from str to int
+            traffic_data['date_day'] = int(traffic_data['date_day'])
+            traffic_data['date_year'] = int(traffic_data['date_year'])
+            traffic_data['date_month'] = int(traffic_data['date_month'])
 
-                # Parse date info from str to int
-                traffic_data['date_day'] = int(traffic_data['date_day'])
-                traffic_data['date_year'] = int(traffic_data['date_year'])
-                traffic_data['date_month'] = int(traffic_data['date_month'])
+            # Create a list with the information per road
+            for road in traffic_data['roads'][0]:
+                information_per_road.append({
+                    'road': road,
+                    'date_year': traffic_data['date_year'][0],
+                    'date_month': traffic_data['date_month'][0],
+                    'date_day': traffic_data['date_day'][0],
+                    'hour': traffic_data['hour'][0]
+                })
 
-                # Create a list with the information per road
-                for road in traffic_data['roads'][0]:
-                    information_per_road.append({
-                        'road': road,
-                        'date_year': traffic_data['date_year'][0],
-                        'date_month': traffic_data['date_month'][0],
-                        'date_day': traffic_data['date_day'][0],
-                        'hour': traffic_data['hour'][0]
-                    })
+            # Parse dict list to dataframe
+            information_per_road = pd.DataFrame.from_dict(information_per_road)
 
-        # Parse dict list to dataframe
-        information_per_road = pd.DataFrame.from_dict(information_per_road)
+            # Store an auxiliary variable for training data as it is parsed when performing the prediction
+            training_information_per_road = copy.copy(information_per_road)
 
-        # Store an auxiliary variable for training data as it is parsed when performing the prediction
-        training_information_per_road = copy.copy(information_per_road)
+            # Retrieve predictions
+            turn_predictions = self._model_predictor.predict(training_information_per_road, num_models=self._num_models)[0]
 
-        # Retrieve predictions
-        turn_predictions = self._model_predictor.predict(training_information_per_road, num_models=self._num_models)[0]
+            # Define a dict for the predicted values per road
+            turn_predictions_per_road = dict()
 
-        # Define a dict for the predicted values per road
-        turn_predictions_per_road = dict()
+            # Iterate over the roads and store the predictions
+            for index, row in information_per_road.iterrows():
+                turn_predictions_per_road[row['road']] = turn_predictions[index].tolist()
 
-        # Iterate over the roads and store the predictions
-        for index, row in information_per_road.iterrows():
-            turn_predictions_per_road[row['road']] = turn_predictions[index].tolist()
-
-        # Publish the message
-        self._mqtt_client.publish(topic=PREDICTION_TOPIC, payload=parse_str_to_valid_schema(turn_predictions_per_road))
+            # Publish the message
+            self._mqtt_client.publish(topic=TURN_PREDICTION_TOPIC+'/'+junction_id,
+                                      payload=parse_str_to_valid_schema(turn_predictions_per_road))
